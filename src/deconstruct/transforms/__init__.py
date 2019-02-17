209c2438df5cbb67afdbbb66e3b73fa78adcd0e1
Richard Rzeszutek <richengguy@users.noreply.github.com>
2019-02-17 18:45:55 -0500

    Added a wavelet visualizer. (#17)

--- src/deconstruct/transforms/__init__.py
+++ src/deconstruct/transforms/__init__.py
@@ -1,10 +1,12 @@
 from deconstruct.transforms.fourier import fourier
 from deconstruct.transforms.corner import corner
 from deconstruct.transforms.hough import hough
+from deconstruct.transforms.wavelet import wavelet
 
 # List of all available transforms.
 TRANSFORMS = {
     'fourier': fourier,
     'corner': corner,
-    'hough': hough
+    'hough': hough,
+    'wavelet': wavelet
 }

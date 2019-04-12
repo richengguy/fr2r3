fb69a4ed2c738d690225a43bd15d316b98d27b8b
Richard Rzeszutek <richengguy@users.noreply.github.com>
2019-04-12 13:06:40 -0400

    Removed unused modules and dependencies. (#28)

--- src/deconstruct/transforms/__init__.py
+++ src/deconstruct/transforms/__init__.py
@@ -1,14 +1,10 @@
-from deconstruct.transforms import structure_tensor
 from deconstruct.transforms.colour import colour
 from deconstruct.transforms.fourier import fourier
 from deconstruct.transforms.hough import hough
-from deconstruct.transforms.wavelet import wavelet
 
 # List of all available transforms.
 TRANSFORMS = {
     'colour': colour,
     'fourier': fourier,
     'hough': hough,
-    'wavelet': wavelet,
-    'structure-tensor': structure_tensor.visualize
 }
